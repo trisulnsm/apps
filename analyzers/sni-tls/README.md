e0f936ded63e260b4706d61ffe40677e196b0453
vivekrajan <vivek@unleashnetworks.com>
2017-07-24 16:35:20 +0530

    SNI app update

--- analyzers/sni-tls/README.md
+++ analyzers/sni-tls/README.md
@@ -1,12 +1,22 @@
 Server Name Indication based metrics
 ====================================
 
-1. A new counter group called "SNI" 
+SNI (Server Name Indicator) is a TLS Extension. This app gives you visibility into 
+TLS/SSL traffic by breaking it up by SNI Hostname. 
+
+
+This app provides three metrics 
+
+1. A new Counter Group called "SNI" 
     1. meter 0 : bandwidth  per hostname
 	2. meter 1 : flows/hits per hostname
 
-2. A new resource group called "SNI" contains IP->SNI hostname mapping 
+2. A new Resource Group called "SNI" contains IP->SNI hostname mapping 
 
-3. Top SNI Hostnames for each meter
+3. Top SNI Hostnames for each meter by bandwidth and hits 
 
 
+CHANGELOG
+=========
+
+0.0.3      Jul-24-2017    SNI names > 64, truncate to 64 for metering 
--- analyzers/sni-tls/sni.lua
+++ analyzers/sni-tls/sni.lua
@@ -61,21 +61,20 @@ TrisulPlugin = {
   reassembly_handler   = {
 
 	onnewflow = function(engine, timestsamp, flowkey)
-	  if not flowkey:id():find("p-01BB") then return end 
-          T.Pimpl[flowkey:id()] =   PDURecord.new(flowkey:id(), TLSSNI.new(flowkey)) 
+	  if flowkey:id():find("p-01BB") then 
+		  T.Pimpl[flowkey:id()] =  PDURecord.new(flowkey:id(), TLSSNI.new(flowkey))
+	  end 
 	end,
 
     -- run the PDU streamer , which will callback into the dissector 
+	-- we only worry about OUT direction (==1)
     onpayload = function(engine, timestamp, flowkey, direction, seekpos, buffer) 
 
-	
-	  if direction==1 and flowkey:id():find("p-01BB") then 
-
+	  if flowkey:id():find("p-01BB") and direction==1 then 
 		  local pdur = T.Pimpl[flowkey:id()] 
 		  pdur.engine=engine
 		  pdur.timestamp=timestamp
 		  pdur:push_chunk(seekpos, buffer:tostring())
-
 	  end
 
     end,
