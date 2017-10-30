0963d8f0d267f1e803e3b9173c646e1f6f95951e
vivekrajan <vivek@unleashnetworks.com>
2017-10-30 19:57:38 +0530

    Firehol enhance reload list every 30 mins

--- analyzers/firehol/firehol.lua
+++ analyzers/firehol/firehol.lua
@@ -14,7 +14,9 @@
 -- 
 
 local FH = require'iprangemap'
-local FIREHOL_FILENAME = "firehol_level1.netset"
+
+local FIREHOL_FILENAME="firehol_level1.netset" 
+local CHECK_SECONDS=1800			
 
 TrisulPlugin = { 
 
@@ -25,13 +27,14 @@ TrisulPlugin = {
 
   -- load the list 
   onload = function()
-  T.fhole = FH.new()
-    local firehol_intel_file  = T.env.get_config("App>DataDirectory") .. "/" ..  FIREHOL_FILENAME
+	T.fhole = FH.new()
+    local firehol_intel_file  = T.env.get_config("App>DataDirectory") .. "/plugins/" ..  FIREHOL_FILENAME
     local status,errormsg = T.fhole:load(firehol_intel_file)
     if status == false then 
       T.logerror("Error loading filehol list msg="..errormsg)
       return false
     end
+	T.last_load=os.time() 
   end,
 
   -- cg_monitor block
@@ -42,15 +45,47 @@ TrisulPlugin = {
     counter_guid = "{4CD742B1-C1CA-4708-BE78-0FCA2EB01A86}",
 
     -- As soon as a new key is seen , new keys repeat every X hours 
+	-- real time
     onnewkey = function(engine, timestamp, key)
       local m = T.fhole:lookup(key)
       if m then 
         T.log("Found IP in FireHOL Blacklist"..key)
         engine:add_alert("{B5F1DECB-51D5-4395-B71B-6FA730B772D9}" ,             
-            nil,"FireHOL",2,"Found alert in FireHOL range "..tostring(m))
+            nil,"FireHOL",2,"IP "..readable_ip(key).." in FireHOL range "..tostring(m))
       end
     end,
+
+    -- onflush - used to check if data transfer happend , major alert 
+	-- near real time 1-minute 
+    onflush = function(engine, timestamp, key, arrayofmetrics )
+      local m = T.fhole:lookup(key)
+      if m then 
+	    local priority = 2 
+		if arrayofmetrics[2] > 0 and arrayofmetrics[3] > 0 then
+			priority=1
+		end
+        T.log("Flush .. Found IP in FireHOL Blacklist"..key)
+        engine:add_alert("{B5F1DECB-51D5-4395-B71B-6FA730B772D9}" ,             
+            nil,"FireHOL",2,"IP "..readable_ip(key).." in FireHOL range "..tostring(m))
+      end
+    end,
+
+	-- beginflush : reload the list every 30 minutes
+	onbeginflush = function(engine,timestamp)
+		if os.time() - T.last_load  > CHECK_SECONDS then 
+			T.last_load=os.time()
+			T.log("Reloading FireHOL list after interval "..CHECK_SECONDS)
+			TrisulPlugin.onload()
+		end
+	end
 
   },
 }
 
+
+-- converts key in trisul format to readable 
+readable_ip = function(key)
+    local pmatch,_, b1,b2,b3,b4= ip_dotted:find("(%x+)%.(%x+)%.(%x+)%.(%x+)")
+    return string.format("%d.%d.%d.%d",tonumber(b1,16)*math.pow(2,24) + tonumber(b2,16)*math.pow(2,16) + tonumber(b3,16)*math.pow(2,8) + tonumber(b4,16)*math.pow(2,0) )
+
+end
