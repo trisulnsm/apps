348f7ce310ef427f8b6834cbf35ec44a98994c72
vivekrajan <vivek@unleashnetworks.com>
2018-03-01 17:45:26 +0530

    TLS Print flow tag..

--- analyzers/tls-print/jahash.lua
+++ analyzers/tls-print/jahash.lua
@@ -26,7 +26,6 @@ ffi.cdef[[
   int MD5_Init(MD5_CTX *c);
   int MD5_Update(MD5_CTX *c, const void *data, size_t len);
   int MD5_Final(unsigned char *md, MD5_CTX *c);
-
 ]]
 
 -- ffi based MD5 
@@ -82,10 +81,10 @@ TrisulPlugin = {
 
     -- check if the required config settings are ON
     local enabled = T.env.get_config("Reassembly>TCPReassembly>Applications>EnableSSLRecordExtraction")
-	  if  enabled:lower() ~= "true" then
-        T.logerror("TLS PRINT: needs the Reassembly>TCPReassembly>Applications>EnableSSLRecordExtraction be TRUE. Cant proceed. ");
-        return false
-	  end
+    if  enabled:lower() ~= "true" then
+      T.logerror("TLS PRINT: needs the Reassembly>TCPReassembly>Applications>EnableSSLRecordExtraction be TRUE. Cant proceed. ");
+      return false
+    end
 
     -- search these paths for fingerprint database 
     local PrintFiles = { 
@@ -118,17 +117,16 @@ TrisulPlugin = {
 
     local cnt=0
     for oneline in f:lines() do
-	  if not oneline:match("^%s*#") then 
-		  local jj = JSON:decode(oneline)
-		  if jj then 
-			T.print_tbl[jj["ja3_hash"]]=jj["desc"]
-			cnt=cnt+1
-		  end
-	  end 
+      if not oneline:match("^%s*#") then 
+        local jj = JSON:decode(oneline)
+        if jj then 
+          T.print_tbl[jj["ja3_hash"]]=jj["desc"]
+          cnt=cnt+1
+        end
+      end 
     end
 
     T.log("Loaded "..cnt.." TLS fingerprints")
-
     f:close()
   end,
 
@@ -156,126 +154,122 @@ TrisulPlugin = {
     -- 
     onattribute = function(engine, timestamp, flowkey, attr_name, attr_value) 
 
-
-    if attr_name == "User-Agent" then
+      if attr_name == "User-Agent" then
         engine:add_flow_edges(flowkey:id(), '{B91A8AD4-C6B6-4FBC-E862-FF94BC204A35}', attr_value:sub(10))
         return
-    end 
-
-
-    if attr_name ~= "TLS:RECORD" then return;  end 
-
-    local payload = SWP.new(attr_value)
-
-    -- Only interested in TLS handshake (type = 22) + client_hello only
-    if payload:next_u8() == 22 and payload:skip(4) and payload:next_u8() == 1 then
-
-      payload:reset()
-      payload:inc(5)
-
-
-      -- per JA3 they want these fields, 
-      -- https://github.com/salesforce/ja3
-      local ja3f = {
-        SSLVersion="",
-        Cipher={},
-        SSLExtension={},
-        EllipticCurve={},
-        EllipticCurvePointFormat={}
-      }
-
-      payload:next_u8()                   -- over handshake_type
-
-      -- validate , sometimes encrypted handshake can misfire 
-      local hslen=payload:next_u24()                  
-      if hslen ~= #attr_value - 9 then return end;
-
-      ja3f.SSLVersion = payload:next_u16() 
-      payload:skip(32)                    -- over client_random
-      payload:skip(payload:next_u8())     -- over SessionID if present 
-
-      ja3f.Cipher = payload:next_u16_arr( payload:next_u16()/2) 
-
-      payload:skip(payload:next_u8())     -- over compression 
-
-      -- extensions, we pick out SNI as well if 
-      payload:push_fence(payload:next_u16())
-
-      local snihostname  = nil 
-
-      while payload:has_more() do
-        local ext_type = payload:next_u16()
-        local ext_len =  payload:next_u16()
-        if ext_type == 10 then
-          ja3f.EllipticCurve  = payload:next_u16_arr( payload:next_u16()/2)
-        elseif ext_type == 11 then
-           ja3f.EllipticCurvePointFormat = payload:next_u8_arr( payload:next_u8())
-        elseif ext_type ==  0 then 
-          payload:push_fence(payload:next_u16())
-          while payload:has_more() do
-            payload:skip(1)
-            snihostname  =  payload:next_str_to_len(payload:next_u16())
-          end
-          payload:pop_fence()
-        else 
-          payload:skip(ext_len)
-        end
-
-        -- skip the padding extension 
-        if ext_type ~= 21 then 
-          ja3f.SSLExtension[#ja3f.SSLExtension+1]=ext_type 
-        end 
-      end
-
-
-      -- kick out GREASE values  from all tables (see RFC) 
-      -- since they can appear at random positions (generating a different hash)
-      -- we need to remove them completely 
-      for _, ja3f_tbl in ipairs( { ja3f.Cipher, ja3f.SSLExtension, ja3f.EllipticCurve} ) do 
-        for i=#ja3f_tbl,1,-1 do
-          if  GREASE_tbl[ja3f_tbl[i]] then table.remove(ja3f_tbl,i);  end
-        end
       end 
+      if attr_name ~= "TLS:RECORD" then return;  end 
+
+      local payload = SWP.new(attr_value)
+
+      -- Only interested in TLS handshake (type = 22) + client_hello only
+      if payload:next_u8() == 22 and payload:skip(4) and payload:next_u8() == 1 then
+
+        payload:reset()
+        payload:inc(5)
+
+        -- per JA3 they want these fields, 
+        -- https://github.com/salesforce/ja3
+        local ja3f = {
+          SSLVersion="",
+          Cipher={},
+          SSLExtension={},
+          EllipticCurve={},
+          EllipticCurvePointFormat={}
+        }
+
+        payload:next_u8()                   -- over handshake_type
+
+        -- validate , sometimes encrypted handshake can misfire 
+        local hslen=payload:next_u24()                  
+        if hslen ~= #attr_value - 9 then return end;
+
+        ja3f.SSLVersion = payload:next_u16() 
+        payload:skip(32)                    -- over client_random
+        payload:skip(payload:next_u8())     -- over SessionID if present 
+
+        ja3f.Cipher = payload:next_u16_arr( payload:next_u16()/2) 
+
+        payload:skip(payload:next_u8())     -- over compression 
+
+        -- extensions, we pick out SNI as well if 
+        payload:push_fence(payload:next_u16())
+
+        local snihostname  = nil 
+
+        while payload:has_more() do
+          local ext_type = payload:next_u16()
+          local ext_len =  payload:next_u16()
+          if ext_type == 10 then
+            ja3f.EllipticCurve  = payload:next_u16_arr( payload:next_u16()/2)
+          elseif ext_type == 11 then
+             ja3f.EllipticCurvePointFormat = payload:next_u8_arr( payload:next_u8())
+          elseif ext_type ==  0 then 
+            payload:push_fence(payload:next_u16())
+            while payload:has_more() do
+              payload:skip(1)
+              snihostname  =  payload:next_str_to_len(payload:next_u16())
+            end
+            payload:pop_fence()
+          else 
+            payload:skip(ext_len)
+          end
+
+          -- skip the padding extension 
+          if ext_type ~= 21 then 
+            ja3f.SSLExtension[#ja3f.SSLExtension+1]=ext_type 
+          end 
+        end
 
 
-      local ja3_str = ja3f.SSLVersion .. "," ..
-               table.concat(ja3f.Cipher,"-")..","..
-               table.concat(ja3f.SSLExtension,"-")..","..
-               table.concat(ja3f.EllipticCurve,"-")..","..
-               table.concat(ja3f.EllipticCurvePointFormat,"-")
-      local ja3_hash = md5sum(ja3_str)
+        -- kick out GREASE values  from all tables (see RFC) 
+        -- since they can appear at random positions (generating a different hash)
+        -- we need to remove them completely 
+        for _, ja3f_tbl in ipairs( { ja3f.Cipher, ja3f.SSLExtension, ja3f.EllipticCurve} ) do 
+          for i=#ja3f_tbl,1,-1 do
+            if  GREASE_tbl[ja3f_tbl[i]] then table.remove(ja3f_tbl,i);  end
+          end
+        end 
 
+        local ja3_str = ja3f.SSLVersion .. "," ..
+                 table.concat(ja3f.Cipher,"-")..","..
+                 table.concat(ja3f.SSLExtension,"-")..","..
+                 table.concat(ja3f.EllipticCurve,"-")..","..
+                 table.concat(ja3f.EllipticCurvePointFormat,"-")
+        local ja3_hash = md5sum(ja3_str)
 
-      print(" flow=".. flowkey:to_s().." hash ".. ja3_hash.. " string="..ja3_str)
+        -- log this useful to create ja3_ database 
+        print(" flow=".. flowkey:to_s().." hash ".. ja3_hash.. " string="..ja3_str)
 
-      -- counters and edges 
-      engine:update_counter('{E8D5E68F-B320-49F3-C83D-66751C3B485F}', ja3_hash, 0, 1)
+        -- counters and edges 
+        engine:update_counter('{E8D5E68F-B320-49F3-C83D-66751C3B485F}', ja3_hash, 0, 1)
 
-      -- see if this is a known hash 
-      local client_desc = T.print_tbl[ja3_hash]
-      if client_desc then
-        engine:update_key_info('{E8D5E68F-B320-49F3-C83D-66751C3B485F}', ja3_hash, client_desc)
-        engine:add_edge('{E8D5E68F-B320-49F3-C83D-66751C3B485F}', ja3_hash, 
-                    '{B91A8AD4-C6B6-4FBC-E862-FF94BC204A35}', client_desc)
-      end
+        -- flow tag 6 chars only 
+        engine:tag_flow(flowkey:id(), ja3_hash:sub(1,6))
 
-      if snihostname  then 
-          -- Streaming Graph Analytics : Add Bi-Directional Edge 
+        -- see if this is a known hash 
+        local client_desc = T.print_tbl[ja3_hash]
+        if client_desc then
+          engine:update_key_info('{E8D5E68F-B320-49F3-C83D-66751C3B485F}', ja3_hash, client_desc)
+          engine:add_edge('{E8D5E68F-B320-49F3-C83D-66751C3B485F}', ja3_hash, 
+                      '{B91A8AD4-C6B6-4FBC-E862-FF94BC204A35}', client_desc)
+        end
+
+        -- add Edge - snihostname can indicate what the server side is, eg Dropbox, Github etc..
+        if snihostname  then 
           engine:add_edge('{E8D5E68F-B320-49F3-C83D-66751C3B485F}', ja3_hash, 
                             '{B91A8AD4-C6B6-4FBC-E862-FF94BC204A35}', snihostname)
-
           engine:add_edge('{B91A8AD4-C6B6-4FBC-E862-FF94BC204A35}', snihostname, 
                   '{E8D5E68F-B320-49F3-C83D-66751C3B485F}', ja3_hash )
-
           engine:add_flow_edges(flowkey:id(), '{B91A8AD4-C6B6-4FBC-E862-FF94BC204A35}', snihostname)
-      else
+        else
           engine:add_flow_edges(flowkey:id(), '{B91A8AD4-C6B6-4FBC-E862-FF94BC204A35}', "No-SNI-Ext")
-      end 
+        end 
 
-      -- Add Flow edges for both SNI and HASH 
-      engine:add_flow_edges(flowkey:id(), '{E8D5E68F-B320-49F3-C83D-66751C3B485F}', ja3_hash)
+        -- Add Flow edges for both SNI and HASH 
+        engine:add_flow_edges(flowkey:id(), '{E8D5E68F-B320-49F3-C83D-66751C3B485F}', ja3_hash)
 
-    end
+      end
     
   end,    
 
