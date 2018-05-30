e8a6ec08dc1a5b187f01548008ac65267a5056ea
dhinesh88 <dhinesh@unleashnetworks.com>
2018-05-30 17:47:55 +0530

    New app edge vetex monitor

--- dashboards/edge-vertex-monitor/edge_vertex_monitor.js
+++ dashboards/edge-vertex-monitor/edge_vertex_monitor.js
@@ -27,7 +27,7 @@ var EdgeVertexMonitor= $.klass({
   },
   //add the form to the dom
   add_form:function(){
-    this.form=$("<form class='form-horizontal'> <div class='row'> <div class='col-xs-6'> <div class='form-group'> <label class='control-label col-xs-4'>Counter Group</label> <div class='col-xs-8'> <select class='cg_id' id='cgguid' name='cgguid'></select> </div> </div> </div> <div class='col-xs-6'> <div class='form-group'> <label class='control-label col-xs-4'>Meter</label> <div class='col-xs-8'> <select class='meter_id' id='meters' multiple name='meter'></select> </div> </div> </div> </div> <div class='row'> <div class='col-xs-6'> <div class='form-group'> <label class='control-label col-xs-4'>Keys</label> <div class='col-xs-8'> <textarea id='keys' name='keys'></textarea> </div> </div> </div> <div class='col-xs-6'> <div class='form-group'> <div id='new_time_selector'></div> </div> </div> </div> <div class='row'> <div class='form-group'> <div class='col-xs-10 col-md-offset-4'> <input id='from_date' name='from_date' type='hidden'> <input id='to_date' name='to_date' type='hidden'> <input class='btn-submit' name='commit' type='submit' value='Get Usage' id='btn_submit'> </div> </div> </div> </form>");
+    this.form=$("<form class='form-horizontal'> <div class='row'> <div class='col-xs-6'> <div class='form-group'> <label class='control-label col-xs-4'>Counter Group</label> <div class='col-xs-8'> <select class='cg_id' id='cgguid' name='cgguid'></select> </div> </div> <div class='form-group'> <label class='control-label col-xs-4'>Keys</label> <div class='col-xs-8'> <textarea id='keys' name='keys'></textarea> <span class='help-block'>comma seperated</span> </div> </div> </div> <div class='col-xs-6'> <div class='form-group'> <label class='control-label col-xs-4'>Meter</label> <div class='col-xs-8'> <select class='meter_id' id='meters' multiple name='meter'></select> </div> </div> <div class='form-group'> <div id='new_time_selector'></div> </div> </div> </div> <div class='row'> <div class='form-group'> <div class='col-xs-10 col-md-offset-4'> <input id='from_date' name='from_date' type='hidden'> <input id='to_date' name='to_date' type='hidden'> <input class='btn-submit' id='btn_submit' name='commit' type='submit' value='Get Usage'>:</input> </div> </div> </div> </form>");
     $(this.domid).append(this.form);
 
     //defaults from local storage
@@ -40,10 +40,10 @@ var EdgeVertexMonitor= $.klass({
     if(localStorage.getItem("/trisul/edge_vertex/monitor/valid_input")==1)
     {
       default_cgguid  = localStorage.getItem("/trisul/edge_vertex/monitor/cgguid");
-      default_meters   = localStorage.getItem("/trisul/edge_vertex/monitor/cgguid");
+      default_meters   = localStorage.getItem("/trisul/edge_vertex/monitor/meters");
       default_keys =  localStorage.getItem("/trisul/edge_vertex/monitor/keys");
       from_date = localStorage.getItem("/trisul/edge_vertex/monitor/from_date");
-      to_date= localStorage.getItem("/trisul/edge_vertex/monitor/top_date")
+      to_date= localStorage.getItem("/trisul/edge_vertex/monitor/to_date")
       tmint = mk_time_interval({from_date:from_date,to_date:to_date});
       tmint = {start_date:from_date,end_date:to_date,start_time_db:tmint.from.tv_sec,end_time_db:tmint.to.tv_sec}
 
@@ -56,6 +56,8 @@ var EdgeVertexMonitor= $.klass({
                             update_input_ids:"#from_date,#to_date",
                             default_ts:tmint,
                             add_class:"pull-right"});
+    $('#new_time_selector').find('.report_range').css("width","100%")
+
     var js_params = {meter_details:this.cg_meter_json,
       selected_cg : default_cgguid,
       selected_st : default_meters,
@@ -69,7 +71,6 @@ var EdgeVertexMonitor= $.klass({
       $('#meters').find('option').prop('selected',true);
     }
 
-    localStorage.setItem("/trisul/edge_vertex/monitor/valid_input",0)
     $(this.domid).append("<div class='clearfix' id='show_key_usage' class='hide' style='padding-top:20px'></div>");
     this.form.submit($.proxy(this.submit_form,this));
   },
@@ -100,26 +101,36 @@ var EdgeVertexMonitor= $.klass({
     this.tris_pg_bar = new TrisProgressBar({max:this.keys.split(",").length,
                                             divid:'show_key_usage',
                                             slim: true });
+
+    var anchor = '<a href="/newdash/index?dash_key=key&guid={{cgguid}}">'
     
     var meters = this.cg_meter_json[this.cgguid];
     var ths = "<th>Readable</th><th>Lable</th>";
-    this.mustache_tmpl = '<td>{{readable}}</td>'+
-                        '<td>{{label}}</td>';
+    this.mustache_tmpl = "<td><a href='/newdash/index?dash_key=key&guid={{cgguid}}&key={{key}}' target='_blank'>{{readable}}</a></td>"+
+                         "<td><a href='/newdash/index?dash_key=key&guid={{cgguid}}&key={{key}}' target='_blank'>{{label}}</a></td>"
     _.each(meters[1],function(meter){
       if(! this.selected_meters.includes(meter[0])){
         return;
       }
-      ths = ths+"<th>"+meter[1]+"</th>";
+      ths = ths+"<th sort='volume'>"+meter[1]+"</th>";
       this.mustache_tmpl = this.mustache_tmpl+"<td>{{stat_"+meter[0]+"}}</td>";
     },this);
     table.append("<thead><tr>"+ths+"</tr></thead>");
     table.append("<tbody></tbody>");
 
     var panel = get_panel_shell({});
-    panel.find('.panel-heading').html('Usage report')
+    var text="Usage report for "+this.cg_meter_json[this.cgguid][0];
+
+    var from_date = new Date(this.form.find('#from_date').val());
+    var duration =  new Date(this.form.find('#to_date').val()).getTime() - from_date.getTime();
+    text = text + " ( " + this.keys.split(",").length + " ) " ;
+    text = text + "<span class='text-info'>"+h_fmtduration(duration/1000,true)+" Ending " + from_date.toString()+"</span>";
+
+    panel.find('.panel-heading').html(text)
     panel.find('.panel-body').append(table);
     $('#show_key_usage').append(panel);
     this.get_usage();
+    table.tablesorter();
 
   },
 
@@ -131,10 +142,14 @@ var EdgeVertexMonitor= $.klass({
     var cthis = this;
     var date = {from_date:this.form.find('#from_date').val(),to_date:this.form.find('#to_date').val()}
     _.each(this.keys.split(","),$.proxy(function(key){
+      var keyt= new TRP.KeyT({label:key});
+      if(localStorage.getItem("/trisul/edge_vertex/monitor/valid_input")==1){
+        keyt =  new TRP.KeyT({key:key})
+      }
       var req=  mk_trp_request(TRP.Message.Command.COUNTER_ITEM_REQUEST,
       {
         counter_group: this.cgguid,
-        key:  new TRP.KeyT({label:key}),
+        key:  keyt,
         time_interval:mk_time_interval(date)
       });
       prom = prom.then( function( f) {
@@ -148,18 +163,14 @@ var EdgeVertexMonitor= $.klass({
   //chain the resp
   get_resp:function(req){
     var cthis = this;
-    var meter_type = cthis.all_meters_type[this.cgguid];
+    var meter_type = cthis.all_meters_type[cthis.cgguid];
     return  get_response(req,function(resp){
-      var o = {readable:resp.key.readable,label:resp.key.label};
-      var v = [];
+      var o = {key:resp.key.key,readable:resp.key.readable,label:resp.key.label,cgguid:cthis.cgguid};
+      var l = _.size(cthis.all_meters_type[cthis.cgguid])
+      var v = new Array(l).fill(0);
       _.each(resp.stats,function(stat){
         _.each(stat.values,function(ai,i){
-          if(v[i]){
             v[i] = v[i] + ai.toNumber();
-          }
-          else{
-            v[i] = ai.toNumber();
-          }
         });
       });
       _.each(v,function(ai,i){
@@ -168,6 +179,7 @@ var EdgeVertexMonitor= $.klass({
           bucket_size = cthis.all_cg_bucketsize[cthis.cgguid].bucket_size;
         }
         o["stat_"+i] = h_fmtvol(ai*bucket_size);
+       
       });
       cthis.data.push(o);
       cthis.update_table();
@@ -206,9 +218,12 @@ var EdgeVertexMonitor= $.klass({
 function run(opts){
   var evm = new EdgeVertexMonitor(opts);
   $(document).ajaxStop(function () {
-    if(evm.form !=undefined && evm.auto_submitted!=true && opts["valid_input"]==1){
+    var valid_input=localStorage.getItem("/trisul/edge_vertex/monitor/valid_input");
+
+    if(evm.form !=undefined && evm.auto_submitted!=true && valid_input==1){
       evm.form.submit();
       evm.auto_submitted = true;
+      localStorage.setItem("/trisul/edge_vertex/monitor/valid_input",0);
     }
   });
 }
@@ -220,19 +235,20 @@ function run(opts){
       .form-group 
         %label.control-label.col-xs-4 Counter Group         
         .col-xs-8 
-          %select.cg_id#cgguid{name:'cgguid'} 
+          %select.cg_id#cgguid{name:'cgguid'}
+
+      .form-group
+        %label.control-label.col-xs-4 Keys
+        .col-xs-8
+          %textarea{name:'keys',id:'keys'}
+          %span.help-block comma seperated
+
     .col-xs-6
       .form-group 
         %label.control-label.col-xs-4 Meter 
         .col-xs-8 
           %select.meter_id#meters{multiple:true,name:'meter'}
-  .row
-    .col-xs-6
-      .form-group
-        %label.control-label.col-xs-4 Keys
-        .col-xs-8
-          %textarea{name:'keys',id:'keys'}
-    .col-xs-6
+  
       .form-group
         #new_time_selector
   .row
