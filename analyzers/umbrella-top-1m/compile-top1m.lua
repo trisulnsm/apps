7321a5a720e43e730c30a133d90077bf1a0de620
vivekrajan <vivek@unleashnetworks.com>
2018-08-31 19:21:47 +0530

    checkcomment in feed

--- analyzers/umbrella-top-1m/compile-top1m.lua
+++ analyzers/umbrella-top-1m/compile-top1m.lua
@@ -3,6 +3,8 @@ local LDB=require'tris_leveldb'
 local csv_file=arg[1]
 local out_db=arg[2]
 
+local nadded=0
+
 print("Compiling 1M "..csv_file) 
 
 -- in_file 
@@ -24,9 +26,14 @@ end
 -- push each  into leveldb  - regex 
 for l in f:lines() do 
 	local h = l:match("%d+[,%s]+(%S+)")
-	ldb:put(h,"1")
+	if h then 
+		ldb:put(h,"1")
+		nadded=nadded+1
+	end
 end 
 
 f:close() 
 ldb:close()
+
+print("  # Added "..nadded.." domains from list")
 
