ddb3b2c25fc3a8ced0f4bf5a9c40be6302941c2a
vivekrajan <vivek@unleashnetworks.com>
2018-09-19 15:45:05 +0530

    new thumbnails

--- dashboards/protocol-tree/new-prototree.js
+++ dashboards/protocol-tree/new-prototree.js
@@ -10,25 +10,56 @@ class NewProtoTree {
 
 	}
 
+	paintHeading(dom) {
+
+		let div = document.createElement('div');
+		div.className += 'col-xs-12 well' 
+		dom.appendChild(div);
+
+		div.innerHTML = `<h2>Protocol tree breakup</h2> \
+			         <h4>Time interval ${new Date(this.fromTS * 1000)} to ${new Date(this.toTS * 1000)} </h4> \
+				 <h4>Total bytes=${Number(this.totals.bytes).toLocaleString()}  packets=${Number(this.totals.packets).toLocaleString()}</h4>`
+
+
+		let ul = document.createElement('ul');
+			ul.className = 'text-danger'
+			ul.innerHTML = `<li><span class="key">protocol</span> \
+			                <span class="metric text-right">bytes  </span>\
+			                <span class="percent text-right"> ( %total) </span> \
+			                <span class="metric text-right">packets</span> \
+			                <span class="percent text-right"> ( %total) </span>\
+			                <span class="bytespkt text-right"> bytes/pkt </span></li>`
+			ul.innerHTML += `<li><span class="key">total counts</span> \
+			                <span class="metric text-right"> ${this.totals.bytes} </span>\
+			                <span class="percent text-right"> ( 100% ) </span> \
+			                <span class="metric text-right"> ${this.totals.packets} </span> \
+			                <span class="percent text-right"> ( 100% ) </span>\
+			                <span class="bytespkt text-right"> ${ (this.totals.bytes/this.totals.packets).toFixed(2)} </span></li>`
+		dom.appendChild(ul);
+	}
 
 	paint(dom,subtree) {
 		let ul = document.createElement('ul');
 		dom.appendChild(ul);
 
 
-		for ( let k of Object.entries(subtree).sort( (a,b) => b[1].bytes - a[1].bytes) )  {
-			if ( k[0] == 'bytes' ||	 k[0] == 'packets') continue; 
+		for ( let k of Object.entries(subtree)
+                              .slice().filter( a => !['bytes','packets'].includes(a[0]) && a[1].bytes>0 && a[1].packets>0 )
+                              .sort( (a,b) => b[1].bytes - a[1].bytes) ) {
 
 			let li = document.createElement('li')
 			li.innerHTML = `<span class="key">${k[0]}</span> \
 			                <span class="metric text-right">${ k[1].bytes} </span>\
 			                <span class="percent text-right"> (${ (k[1].bytes/this.totals.bytes*100).toFixed(2)} %)</span> \
 			                <span class="metric text-right">${ k[1].packets}</span> \
-			                <span class="percent text-right"> (${ (k[1].packets/this.totals.packets*100).toFixed(2)} %)</span>`
+			                <span class="percent text-right"> (${ (k[1].packets/this.totals.packets*100).toFixed(2)} %)</span>\
+			                <span class="bytespkt text-right"> ${ (k[1].bytes/k[1].packets).toFixed(2)}</span>`
 			ul.appendChild(li);
 
 			if (k[1].nodetype !='leaf') {
 				this.paint(li,k[1])
+			} else {
+				li.className="text-info"
 			}
 		}
 
@@ -37,9 +68,10 @@ class NewProtoTree {
 
 	layout() {
 		document.querySelectorAll(`${this.divid} ul`).forEach( (f) => {f.style.fontFamily='monospace'} )
-		document.querySelectorAll(`${this.divid} span.metric`).forEach( (f) => {f.style.minWidth='200px';f.style.display='inline-block'} )
-		document.querySelectorAll(`${this.divid} span.key`).forEach( (f) => {f.style.minWidth='150px';f.style.display='inline-block'} )
-		document.querySelectorAll(`${this.divid} span.percent`).forEach( (f) => {f.style.minWidth='30px';f.style.display='inline-block'} )
+		document.querySelectorAll(`${this.divid} span.metric`).forEach( (f) => {f.style.minWidth='150px';f.style.display='inline-block'} )
+		document.querySelectorAll(`${this.divid} span.key`).forEach( (f) => {f.style.minWidth='120px';f.style.display='inline-block'} )
+		document.querySelectorAll(`${this.divid} span.bytespkt`).forEach( (f) => {f.style.minWidth='120px';f.style.display='inline-block'} )
+		document.querySelectorAll(`${this.divid} span.percent`).forEach( (f) => {f.style.minWidth='80px';f.style.display='inline-block'} )
 	}
 
 
@@ -54,10 +86,10 @@ class NewProtoTree {
 
 		// get total available timewindow 
 		let  ts= await fetch_trp(TRP.Message.Command.TIMESLICES_REQUEST, {
-			get_totalwindow: true
+			get_total_window: true
 		});
-		this.fromTS = parseInt(ts.slices[0].time_interval.from.tv_sec)
-		this.toTS = parseInt(ts.slices[0].time_interval.to.tv_sec)
+		this.fromTS = parseInt(ts.total_window.from.tv_sec)
+		this.toTS = parseInt(ts.total_window.to.tv_sec)
 
 
 		// Find "Protocol Tree" counter group 
@@ -87,6 +119,7 @@ class NewProtoTree {
 		this.totals= this.tree['SYS:GROUP_TOTALS']
 		delete this.tree['SYS:GROUP_TOTALS']
 
+		this.paintHeading(document.querySelector(this.divid));
 		this.paint(document.querySelector(this.divid), this.tree);
 		this.layout()
 
@@ -114,3 +147,5 @@ function run(opts)
 {
 	new NewProtoTree(opts).run() 	
 }
+
+//# sourceURL=new-prototree.js
