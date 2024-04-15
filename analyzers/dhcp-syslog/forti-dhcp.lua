4117a63b2b7efb6dd49e6813bb9a0f4a7fd12bfe
vivek <vivek@unleashnetworks.com>
2024-04-15 16:16:50 +0530

    minor fix : set flow counter to true by default to it can work with netflow

--- analyzers/dhcp-syslog/forti-dhcp.lua
+++ analyzers/dhcp-syslog/forti-dhcp.lua
@@ -40,7 +40,7 @@ TrisulPlugin = {
     protocol_guid = "{4323003E-D060-440B-CA26-E146C0C7DB4E}", 
 
 	-- also work in NETFLOW_TAP mode
-	-- flow_counter = true,
+	flow_counter = true,
 
     onpacket = function(engine,layer)
       local syslogstr = layer:rawbytes():tostring()
