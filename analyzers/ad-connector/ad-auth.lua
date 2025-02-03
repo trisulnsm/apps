748fb88f0636ecab04da3e922925687f7db1c018
vivek <vivek@unleashnetworks.com>
2025-02-03 18:24:59 +0530

    tweaks

--- analyzers/ad-connector/ad-auth.lua
+++ analyzers/ad-connector/ad-auth.lua
@@ -6,6 +6,9 @@ function to_ipkey_format(dotted_ip)
     return string.format("%02X.%02X.%02X.%02X", b1, b2, b3, b4)
 end
 
+local dbg=require'debugger'
+
+
 TrisulPlugin = {
 
     -- the ID block, you can skip the fields marked 'optional '
@@ -18,8 +21,12 @@ TrisulPlugin = {
     -- COMMON FUNCTIONS:  onload, onunload, onmessage
     --
     onload = function() 
+		local kRexLogon = '^<\\d+>([A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}).+\\s4624\\s.+An account was successfully logged on.+?Account Name:\\s+(\\w[\\w-]+).+?Account Domain:\\s+(\\w+).+?Workstation Name:\\s+([\\w-]+).+?Source Network Address: (\\d{1,3}(?:\\.\\d{1,3}){3})';
 
-		T.regex_kerb_logon  = T.re2('^<\\d+>([A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}).+An account was successfully logged on.+?Account Name:\\s+(\\w+).+?Account Domain:\\s+(\\w+).+?Workstation Name: (\\S+).+?Source Network Address: (\\d{1,3}(?:\\.\\d{1,3}){3})')
+		T.regex_kerb_logon  = T.re2(kRexLogon)
+
+		print(kRexLogon);
+
 		T.regex_kerb_logoff  = T.re2('^<\\d+>([A-Za-z]{3}\\s+[0-9]{2}\\s+[0-9]{2}:[0-9]{2}:[0-9]{2}).+An account was logged off.+?Account Name:\\s+(\\w+).+?Account Domain:\\s+(\\w+)')
 
 
@@ -45,6 +52,7 @@ TrisulPlugin = {
 
 				-- print( "++" .. ts.." " .. adomain .. "/".. username .. " " .. wsname .. " "  .. ipaddress) 
 
+
 				local aduser = adomain.."/"..username 
 
                 engine:post_message_frontend(
@@ -52,12 +60,16 @@ TrisulPlugin = {
                     "USERLOGON|"..ipaddress .."|"..aduser 
                 )
 
-                engine:post_message_frontend(
-                    "{1A41BB0C-A872-46E2-DF19-85B658643B58}",
-                    "MACHINENAME|"..ipaddress.."|"..wsname
-                )
+				if wsname ~= "-" then 
 
-                engine:update_key_info("{4CD742B1-C1CA-4708-BE78-0FCA2EB01A86}", to_ipkey_format(ipaddress), wsname)
+					engine:post_message_frontend(
+						"{1A41BB0C-A872-46E2-DF19-85B658643B58}",
+						"MACHINENAME|"..ipaddress.."|"..wsname
+					)
+
+					engine:update_key_info("{4CD742B1-C1CA-4708-BE78-0FCA2EB01A86}", to_ipkey_format(ipaddress), wsname)
+
+				end 
 
 				engine:add_resource( "{E3EF6B1A-6553-4474-69B6-D015CEF3D41F}", 
 									 layer:packet():flowid():id(), "AD AUTH SYSLOG", syslogstr)
