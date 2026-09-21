a92567bc749b57b17a83659dcd658137ad084b30
Vivek Rajagopal <vivek@unleashnetworks.com>
2026-09-21 18:27:04 +0530

    Error handling IPv6 Home Networks

--- analyzers/tcp-analyzer/tcp_analyzer.lua
+++ analyzers/tcp-analyzer/tcp_analyzer.lua
@@ -1,4 +1,6 @@
-local bit=require'bit' 
+local bit=require'bit'
+
+local CG = "{E45623ED-744C-4053-1401-84C72EE49D3B}"
 
 TrisulPlugin = {
 
@@ -9,7 +11,7 @@ TrisulPlugin = {
 
   countergroup = {
     control = {
-      guid = "{E45623ED-744C-4053-1401-84C72EE49D3B}",
+      guid = CG,
       name = "TCP Analyzer",
       description = "Measure setup time retrans timeouts",
       bucketsize = 60,
@@ -25,53 +27,52 @@ TrisulPlugin = {
       {  6, T.K.vartype.COUNTER,  20, 20, "flows","Poor Quality Flows",     "flws" },
       {  7, T.K.vartype.COUNTER,  20, 20, "flows","Timeouts",               "flws" },
       {  8, T.K.vartype.COUNTER,  20, 20, "flows","Unidirectional",         "flws" },
-    },  
+    },
   },
 
   sg_monitor  = {
 
     onflush = function(engine, newflow)
 
-      local flowkey = newflow:flow() 
+      local flowkey = newflow:flow()
 
-      if flowkey:protocol() ~= "06" then return end 
+      if flowkey:protocol() ~= "06" then return end
 
-      local retrans_rate = 100*newflow:retransmissions()/(newflow:az_packets()+newflow:az_packets());
+      -- addresses in Trisul key form - no formatting, no reparsing, and the
+      -- same code path for IPv4 and IPv6
+      local ipa = flowkey:ipa()
+      local ipz = flowkey:ipz()
+
+      local packets       = newflow:az_packets() + newflow:za_packets()
+      local retrans       = newflow:retransmissions()
+      local retrans_rate  = packets > 0 and 100*retrans/packets or 0
+      local setup_rtt     = newflow:setup_rtt()
+
       -- high retrans
-      if retrans_rate  > 5  then 
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipa() , 6, 1)
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipz() , 6, 1)
+      if retrans_rate  > 5  then
+        engine:update_counter(CG, ipa, 6, 1)
+        engine:update_counter(CG, ipz, 6, 1)
         newflow:add_tag("BADQUALITY")
-      end 
+      end
 
-      -- timout : did not terminate with RST/FIN 
+      -- timout : did not terminate with RST/FIN
       if bit.band(newflow:state(),0x0100) ~= 0 then
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipa() , 7, 1)
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipz() , 7, 1)
+        engine:update_counter(CG, ipa, 7, 1)
+        engine:update_counter(CG, ipz, 7, 1)
         newflow:add_tag("BADQUALITY")
       end
 
-      if T.host:is_homenet(flowkey:ipa_readable() ) then
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipa() , 0, newflow:setup_rtt())
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipa() , 2, newflow:retransmissions())
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipa() , 4, retrans_rate)
-      else
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipa() , 1, newflow:setup_rtt())
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipa() , 3, newflow:retransmissions())
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipa() , 5, retrans_rate)
-      end
+      -- meters 0,2,4 are the Internal (homenet) set, 1,3,5 the External one
+      local a = T.host:is_homenet_key(ipa) and 0 or 1
+      engine:update_counter(CG, ipa, 0+a, setup_rtt)
+      engine:update_counter(CG, ipa, 2+a, retrans)
+      engine:update_counter(CG, ipa, 4+a, retrans_rate)
 
-      if T.host:is_homenet(flowkey:ipz_readable()) then
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipz(), 0, newflow:setup_rtt())
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipz(), 2, newflow:retransmissions())
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipz() ,4, retrans_rate)
-      else
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipz(), 1, newflow:setup_rtt())
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipz(), 3, newflow:retransmissions())
-        engine:update_counter("{E45623ED-744C-4053-1401-84C72EE49D3B}", flowkey:ipz() ,5, retrans_rate)
-      end
+      local z = T.host:is_homenet_key(ipz) and 0 or 1
+      engine:update_counter(CG, ipz, 0+z, setup_rtt)
+      engine:update_counter(CG, ipz, 2+z, retrans)
+      engine:update_counter(CG, ipz, 4+z, retrans_rate)
 
     end,
   },
 }
-
